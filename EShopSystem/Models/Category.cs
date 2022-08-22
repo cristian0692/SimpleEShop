a633320c4add8174d04d464db16ffb0fdd35f2e8
Bolohan Cristian <cr.bolohan@gmail.com>
2022-08-22 16:03:46 +0300

    Add 3 models,Product,category and Customer

---
+++ EShopSystem/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopSystem.Models
+{
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ EShopSystem/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopSystem.Models
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerMobile { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAddress { get; set; }
+        public float CustomerBalance { get; set; }
+    }
+}
